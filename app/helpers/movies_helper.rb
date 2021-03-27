c097215019c365b821cf77667f255e1af1fc7658
Mark's Desktop <superiorseven421@gmail.com>
2021-03-27 18:02:45 -0500

    helpers 07b

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+    def format_total_gross(movie)
+        if movie.total_gross < 500000000
+            content_tag(:strong, "FLOP!")
+        else
+            number_to_currency(movie.total_gross)
+        end
+    end
 end
