4ec9f48f0f66e9cfd8a911a3d7a0c59797b3c12a
Mark's Laptop <superiorseven421@gmail.com>
2021-05-28 16:50:57 -0500

    Fixed formatting and the likes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,20 @@
 class MoviesController < ApplicationController
-    def index
-        @movies = Movie.all
-    end
+  def index
+    @movies = Movie.all
+  end
 
-    def show
-        @movie = Movie.find(params[:id])
-    end
+  def show
+    @movie = Movie.find(params[:id])
+  end
 
-    def edit
-        @movie = Movie.find(params[:id])
-    end
+  def edit
+    @movie = Movie.find(params[:id])
+  end
 
-    def update
-        @movie = Movie.find(params[:id])
-        movie_params = params.require(:movie).permit(:title, :rating, :total_gross, :released_on, :description)
-        @movie.update(movie_params)
-        redirect_to @movie
-    end
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title, :rating, :total_gross, :released_on, :description)
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
 end
